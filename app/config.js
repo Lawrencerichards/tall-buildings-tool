c197ca7f39b5f92ba36f57e6969ab11d049e8e2f
lawrence richards <lawrence.richards@unboxed.co>
2020-07-08 18:39:00 +0100

    finishing touches

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Tall buildings survey prototype',
+  serviceName: 'Building safety platform',
 
   // Default port that prototype runs on
   port: '3000',
